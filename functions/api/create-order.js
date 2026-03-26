b109da30e67aab6710665f09b235eeda18d18ef2
KUACK <grzegorzloban@gmail.com>
2026-03-26 16:32:56 +0100

    upload poprawki

--- functions/api/create-order.js
+++ functions/api/create-order.js
@@ -76,7 +76,7 @@ export async function onRequestPost({ request, env }) {
       discountFactor = 0.65;
     }
   } else if (promo === "naskale") {
-    const deadline = new Date("2026-03-30T00:00:00+01:00");
+    const deadline = new Date("2026-04-08T00:00:00+01:00");
     if (now < deadline) {
       discountFactor = 0.5;
     }
@@ -85,7 +85,7 @@ export async function onRequestPost({ request, env }) {
     if (now < deadline) {
       discountFactor = 0.5;
     }
-  } else if (promo === "ligotzki022725@") {
+  } else if (promo === "asknet12#") {
     fixedPriceGrosze = 200;
   }
 
